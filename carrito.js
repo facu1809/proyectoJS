318b56059fad51cca1cfb58e4a16c36a64f42a6e
Facundo <facu1809@gmail.com>
2026-01-12 23:12:14 +0100

    segunda entrega

--- carrito.js
+++ carrito.js
@@ -1,3 +1,15 @@
+function saludar(nombre, apellido) {
+  console.log("Hola " + nombre + " " + apellido);
+}
+
+let nombreUser = prompt("Ingrese su nombre");
+let apellidoUser = prompt("Ingrese su apellido");
+alert ("Hola " + nombreUser + " " + apellidoUser + " " + " Bienvenido a ALLTIME")
+
+saludar(nombreUser, apellidoUser);
+
+let iniciar = confirm ("¿Quieres comprar relojes?")
+
 // Traer carrito del localStorage
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
@@ -12,6 +24,7 @@ function agregar(marca, precio) {
   guardar();
   mostrar();
 }
+console.log (carrito)
 
 // Mostrar carrito
 function mostrar() {
